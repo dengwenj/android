cf0060c1bc3519b1231124ebd412086e593cf4fc
朴睦 <927561237@qq.com>
2024-06-27 19:27:34 +0800

    登录逻辑

--- middle_control/src/main/java/vip/dengwj/LoginActivity.java
+++ middle_control/src/main/java/vip/dengwj/LoginActivity.java
@@ -1,21 +1,39 @@
 package vip.dengwj;
 
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
-import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class LoginActivity extends AppCompatActivity implements RadioGroup.OnCheckedChangeListener {
+import java.util.Random;
+
+import vip.dengwj.Config.HideTextWatcher;
+
+public class LoginActivity extends AppCompatActivity
+        implements RadioGroup.OnCheckedChangeListener, View.OnClickListener {
+    private static final String password = "111111";
+
     private TextView passwordLabel;
     private TextView forgetPassword;
     private CheckBox rememberPassword;
-    private Button login;
     private EditText edittextPassword;
+    private EditText edittextPhone;
+    private RadioButton passwordLogin;
+    private RadioButton verifyCodeLogin;
+    private String verifyCode;
+    private ActivityResultLauncher<Intent> intentActivityResultLauncher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,15 +42,34 @@ public class LoginActivity extends AppCompatActivity implements RadioGroup.OnChe
 
         RadioGroup radioGroup = findViewById(R.id.radio);
         radioGroup.setOnCheckedChangeListener(this);
-        findViewById(R.id.password_login);
-        findViewById(R.id.verify_code_login);
+        passwordLogin = findViewById(R.id.password_login);
+        verifyCodeLogin = findViewById(R.id.verify_code_login);
 
         findViewById(R.id.edittext_phone);
         passwordLabel = findViewById(R.id.password_label);
+        edittextPhone = findViewById(R.id.edittext_phone);
         edittextPassword = findViewById(R.id.edittext_password);
         forgetPassword = findViewById(R.id.forget_password);
         rememberPassword = findViewById(R.id.remember_password);
-        login =findViewById(R.id.login);
+        findViewById(R.id.login).setOnClickListener(this);
+        forgetPassword.setOnClickListener(this);
+
+        // 键盘自动隐藏
+        edittextPhone.addTextChangedListener(new HideTextWatcher(edittextPhone, 11, LoginActivity.this));
+        edittextPassword.addTextChangedListener(new HideTextWatcher(edittextPassword, 6, LoginActivity.this));
+
+        // 下一个页面返回上一个页面传递数据
+        intentActivityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                this::onActivityResult
+        );
+    }
+
+    /**
+     * 忘记密码页切换回来的回调函数
+     */
+    private void onActivityResult(ActivityResult result) {
+
     }
 
     // tab 切换
@@ -42,14 +79,115 @@ public class LoginActivity extends AppCompatActivity implements RadioGroup.OnChe
         if (checkedId == R.id.password_login) {
             passwordLabel.setText(getString(R.string.login_password));
             edittextPassword.setHint(getString(R.string.input_password));
+            edittextPassword.setInputType(0x00000012);
+            edittextPassword.setText(null);
             forgetPassword.setText(getString(R.string.forget_password));
             rememberPassword.setVisibility(View.VISIBLE);
         } else if (checkedId == R.id.verify_code_login) {
             // 验证码登录
             passwordLabel.setText(getString(R.string.verifycode));
             edittextPassword.setHint(getString(R.string.input_verifycode));
+            edittextPassword.setInputType(InputType.TYPE_CLASS_NUMBER);
+            edittextPassword.setText(null);
             forgetPassword.setText(getString(R.string.get_verifycode));
             rememberPassword.setVisibility(View.GONE);
         }
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+
+        // 点击登录
+        if (id == R.id.login) {
+            handleLogin();
+        } else if (id == R.id.forget_password) {
+            // 忘记密码
+            if (passwordLogin.isChecked()) {
+                // 跳转到忘记密码页
+                Intent intent = new Intent(this, ForgetPasswordActivity.class);
+                intent.putExtra("phone", edittextPhone.getText().toString());
+                intentActivityResultLauncher.launch(intent);
+            } else {
+                // 获取验证码
+                getVerifyCode();
+            }
+        }
+    }
+
+    /**
+     * 获取验证码
+     */
+    private void getVerifyCode() {
+        // 随机 6 位
+        Random random = new Random();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 6; i++) {
+            int r = random.nextInt(10);
+            sb.append(r);
+        }
+        verifyCode = sb.toString();
+
+        new AlertDialog.Builder(this)
+                .setTitle("验证码")
+                .setMessage("手机号为：" + edittextPhone.getText().toString() + "用户的验证码是" + verifyCode)
+                .setPositiveButton("记住了", null)
+                .show();
+    }
+
+    /**
+     * 点击登录逻辑
+     */
+    private void handleLogin() {
+        if (!rules()) {
+            return;
+        }
+
+        // 说明验证通过
+        String phone = edittextPhone.getText().toString();
+        String psd = edittextPassword.getText().toString();
+        // 说明是密码登录
+        if (passwordLogin.isChecked()) {
+            // 密码错误
+            if (!LoginActivity.password.equals(psd)) {
+                toastMsg("密码错误");
+                return;
+            }
+        } else {
+            // 说明是验证码登录
+            if (!psd.equals(verifyCode)) {
+                toastMsg("验证码错误");
+                return;
+            }
+        }
+
+        new AlertDialog.Builder(this)
+                .setTitle("提示")
+                .setMessage("手机号为:" + edittextPhone.getText().toString() + "的用户登录成功")
+                .setPositiveButton("退出App", (dialog, which) -> finish())
+                .setNegativeButton("我再看看", null)
+                .show();
+    }
+
+    /**
+     * 手机号密码验证码验证
+     */
+    private boolean rules() {
+        if (edittextPhone.getText().toString().length() < 11) {
+            toastMsg("请输入11位手机号");
+            return false;
+        }
+
+        if (edittextPassword.getText().toString().length() < 6) {
+            String str = passwordLogin.isChecked() ? "密码" : "验证码";
+            toastMsg("请输入6位" + str);
+            return false;
+        }
+
+        return true;
+    }
+
+    private void toastMsg(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
 }
