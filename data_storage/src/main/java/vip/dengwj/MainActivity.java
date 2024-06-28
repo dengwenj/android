0de2ad10b6a1a8b0f66d991e38bee46e22b54099
朴睦 <927561237@qq.com>
2024-06-28 15:02:21 +0800

    SharedPreferences 的用法

---
+++ data_storage/src/main/java/vip/dengwj/MainActivity.java
@@ -0,0 +1,14 @@
+package vip.dengwj;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
