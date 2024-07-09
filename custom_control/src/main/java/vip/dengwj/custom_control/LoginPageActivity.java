4abd29192d9da411961783eab35d7cc9cc4cf5f6
朴睦 <927561237@qq.com>
2024-07-09 19:00:35 +0800

    自定义键盘 -> 事件处理

--- custom_control/src/main/java/vip/dengwj/custom_control/LoginPageActivity.java
+++ custom_control/src/main/java/vip/dengwj/custom_control/LoginPageActivity.java
@@ -1,14 +1,31 @@
 package vip.dengwj.custom_control;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class LoginPageActivity extends AppCompatActivity {
+import vip.dengwj.custom_control.customView.loginPage.LoginKeyBoard;
+
+public class LoginPageActivity extends AppCompatActivity
+        implements LoginKeyBoard.OnKeyPressListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+
+        LoginKeyBoard loginKeyBoard = findViewById(R.id.login_key_board);
+        loginKeyBoard.setOnKeyPressListener(this);
+    }
+
+    @Override
+    public void onNumberPress(int number) {
+        Log.d("pumu", "number:" + number);
+    }
+
+    @Override
+    public void onBackPress() {
+        Log.d("pumu", "back");
     }
 }
--- custom_control/src/main/java/vip/dengwj/custom_control/customView/loginPage/LoginKeyBoard.java
+++ custom_control/src/main/java/vip/dengwj/custom_control/customView/loginPage/LoginKeyBoard.java
@@ -6,12 +6,17 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
 import vip.dengwj.custom_control.R;
 
-public class LoginKeyBoard extends LinearLayout {
+public class LoginKeyBoard extends LinearLayout implements View.OnClickListener {
+    private int numKey;
+
+    private OnKeyPressListener onKeyPressListener;
+
     public LoginKeyBoard(Context context) {
         this(context, null);
     }
@@ -23,10 +28,46 @@ public class LoginKeyBoard extends LinearLayout {
     public LoginKeyBoard(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
 
-        View view = LayoutInflater.from(context).inflate(R.layout.num_key_pad, this);
+        LayoutInflater.from(context).inflate(R.layout.num_key_pad, this);
 
-        view.findViewById(R.id.number_0).setOnClickListener((v) -> {
-            Log.d("pumu", "点击");
-        });
+        findViewById(R.id.number_1).setOnClickListener(this);
+        findViewById(R.id.number_2).setOnClickListener(this);
+        findViewById(R.id.number_3).setOnClickListener(this);
+        findViewById(R.id.number_4).setOnClickListener(this);
+        findViewById(R.id.number_5).setOnClickListener(this);
+        findViewById(R.id.number_6).setOnClickListener(this);
+        findViewById(R.id.number_7).setOnClickListener(this);
+        findViewById(R.id.number_8).setOnClickListener(this);
+        findViewById(R.id.number_9).setOnClickListener(this);
+        findViewById(R.id.number_0).setOnClickListener(this);
+        findViewById(R.id.number_back).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+
+        // 删除键
+        if (id == R.id.number_back) {
+            if (onKeyPressListener == null) return;
+            onKeyPressListener.onBackPress();
+        } else {
+            // 数字键
+            numKey = Integer.parseInt(((TextView) v).getText().toString());
+
+            if (onKeyPressListener != null) {
+                onKeyPressListener.onNumberPress(numKey);
+            }
+        }
+    }
+
+    public void setOnKeyPressListener(OnKeyPressListener listener) {
+        onKeyPressListener = listener;
+    }
+
+    public interface OnKeyPressListener {
+        void onNumberPress(int number);
+
+        void onBackPress();
     }
 }
