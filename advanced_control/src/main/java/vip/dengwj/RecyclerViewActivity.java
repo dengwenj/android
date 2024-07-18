df20c9d9964cf2f0b7b2e3ffa00ba0101653cc3f
朴睦 <927561237@qq.com>
2024-07-18 14:19:52 +0800

    布局管理器设置

--- advanced_control/src/main/java/vip/dengwj/RecyclerViewActivity.java
+++ advanced_control/src/main/java/vip/dengwj/RecyclerViewActivity.java
@@ -19,6 +19,12 @@ import vip.dengwj.domain.RecyclerItem;
 /**
  * RecyclerView 是 Android 的一个高级 UI 组件，用于展示大量数据。
  * 它可以自动回收不可见的视图，并且可以使用不同的布局管理器来实现不同的布局。
+ *
+ * 1、通过 findViewById 找打控件
+ * 2、准备好数据
+ * 3、设置布局管理器
+ * 4、创建适配器
+ * 5、设置适配器
  */
 public class RecyclerViewActivity extends AppCompatActivity {
     private List<RecyclerItem> list = new ArrayList<>();
@@ -34,6 +40,8 @@ public class RecyclerViewActivity extends AppCompatActivity {
 
         // 初始化数据
         initData();
+
+        showList(true, false);
     }
 
     private void initData() {
@@ -42,8 +50,13 @@ public class RecyclerViewActivity extends AppCompatActivity {
             recyclerItem.setTitle("第" + i + "行");
             list.add(recyclerItem);
         }
+    }
 
+    private void showList(boolean isVertical, boolean isReverse) {
+        // 布局管理器
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setOrientation(isVertical ? RecyclerView.VERTICAL : RecyclerView.HORIZONTAL);
+        layoutManager.setReverseLayout(isReverse);
         recyclerView.setLayoutManager(layoutManager);
         // 适配器
         RecyclerAdapter recyclerAdapter = new RecyclerAdapter(list);
@@ -62,13 +75,13 @@ public class RecyclerViewActivity extends AppCompatActivity {
         // 转成成员变量 command + option + f
         int itemId = item.getItemId();
         if (itemId == R.id.list_view_demo1) {
-            Log.d("pumu", "list_view_demo1");
+            showList(true, false);
         } else if (itemId == R.id.list_view_demo2) {
-
+            showList(true, true);
         } else if (itemId == R.id.list_view_demo3) {
-
+            showList(false, false);
         } else if (itemId == R.id.list_view_demo4) {
-
+            showList(false, true);
         } else if (itemId == R.id.grid_view_demo1) {
 
         } else if (itemId == R.id.grid_view_demo2) {
