f99cb5a1f1c27e964e74315d5176d26092c77709
朴睦 <927561237@qq.com>
2024-09-03 17:50:31 +0800

    ui 书写

--- service/src/main/java/vip/dengwj/service/BankActivity.java
+++ service/src/main/java/vip/dengwj/service/BankActivity.java
@@ -2,6 +2,7 @@ package vip.dengwj.service;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -17,5 +18,17 @@ public class BankActivity extends AppCompatActivity {
         // 显示意图绑定
         // Intent intent = new Intent(this, BankService.class);
         // bindService();
+    }
+
+    public void handleNormalUserClick(View view) {
+        startActivity(new Intent(this, NormalUserActivity.class));
+    }
+
+    public void handleBankWorkerClick(View view) {
+        startActivity(new Intent(this, BankWorkerActivity.class));
+    }
+
+    public void handleBankBossClick(View view) {
+        startActivity(new Intent(this, BankBossActivity.class));
     }
 }
---
+++ service/src/main/java/vip/dengwj/service/BankBossActivity.java
@@ -0,0 +1,14 @@
+package vip.dengwj.service;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BankBossActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bank_boss);
+    }
+}
