c796aee6da757af409020b471dc19261ef00086d
朴睦 <927561237@qq.com>
2024-09-04 17:04:33 +0800

    服务的开启方式有两种

--- service/src/main/java/vip/dengwj/service/services/FirstService.java
+++ service/src/main/java/vip/dengwj/service/services/FirstService.java
@@ -11,6 +11,19 @@ import androidx.annotation.Nullable;
 
 import vip.dengwj.service.actions.interfaces.ICommunication;
 
+/**
+ * 服务的开启方式有两种：
+ * 1、startService() 开启服务 ---> stopService() 停止服务
+ * 优点：服务可以长期于后台运行，缺点：不能够进行通讯
+ * 生命周期：
+ * 最基本的生命周期：onCreate() -> onStartCommand() -> onDestroy()
+ * 多次启动服务的生命周期，如果服务已经启动时，那么就不会再走 onCreate 方法了。除非执行了 onDestroy()
+ * onCreate() -> onStartCommand() -...-> onStartCommand() -> onDestroy()
+ *
+ * 2、bindService() 绑定服务，如果服务没有启动，自动启动 -> unBindService() 解绑服务
+ * 优点：可以进行通讯，缺点：不可以长期于后台运行，如果不解绑则会发生泄漏，如果解绑了，服务将停止运行
+ * 生命周期：onCreate() -> onBind() -> onUnbind() -> onDestroy()
+ */
 public class FirstService extends Service {
     public static final String TAG = "pumu";
 
